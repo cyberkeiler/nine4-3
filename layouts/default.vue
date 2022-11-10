02272b5e9094a38bbe29f86bf5d1c67cdeb326ea
cyberkeiler <tobias@sendelba.ch>
2022-11-10 11:39:31 +0000

    Redefine as default layout

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-white bg-black">
+    <Header />
+    <slot />
+    <Footer />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Home: nine4",
+      meta: [{ name: "description", content: "Welcome to nine4 homepage." }],
+    },
+  },
 });
